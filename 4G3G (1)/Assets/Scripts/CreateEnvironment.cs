deea69a3212bd47f06cd8d7ef1bd1fc949078a10
andreabefuen <al341488@uji.es>
2019-03-15 13:10:46 +0100

    Starting project

---
+++ 4G3G (1)/Assets/Scripts/CreateEnvironment.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateEnvironment : MonoBehaviour
+{
+
+
+    public float offset;
+
+    public GameObject prefabNode;
+
+    public Node[,] matrixNodes;
+
+    Vector3 startPos = new Vector3(0, 0, 0);
+
+    public Vector2 gridWorldSize;
+    public float nodeRadius;
+    int gridSizeX, gridSizeY;
+    float nodeDiameter;
+
+    private int rows, columns;
+    private GameObject grid;
+
+
+    private void Awake()
+    {
+        nodeDiameter = nodeRadius * 2;
+
+        gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+
+        rows = gridSizeX;
+        columns = gridSizeY;
+
+        prefabNode.gameObject.transform.localScale = new Vector3(nodeDiameter, prefabNode.transform.localScale.y, nodeDiameter);
+
+        grid = GameObject.Find("Grid");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        matrixNodes = new Node[rows, columns];
+
+        for (int f = 1; f < rows; f++)
+        {
+            for (int c = 1; c < columns; c++)
+            {
+                GameObject aux = Instantiate(prefabNode);
+                aux.transform.parent = grid.transform;
+                aux.transform.position = startPos;
+                startPos = new Vector3((offset + nodeDiameter)*c, 0 , startPos.z);
+
+                matrixNodes[f, c] = new Node(aux.transform.position, f, c);
+
+            }
+
+            startPos = new Vector3(0, 0, (offset + nodeDiameter)* f);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 4G3G (1)/Assets/Scripts/Node.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    //public GameObject nodeGameobject;
+    public Transform nodeTransform;
+    public Vector3 worldPosition;
+
+    public GameObject objectInNode; //The thing that is on the node (building, solar panel, etc)
+
+    public List<Node> neighbours; //The neighbours of the node
+
+
+    public int gridX, gridY;
+
+
+
+    public Node(Vector3 _worldPos, int _gridX, int _gridY)
+    {
+        //nodeGameobject = this.gameObject;
+        _worldPos = worldPosition;
+        _gridX = gridX;
+        _gridY = gridY;
+
+        objectInNode = null;
+    }
+
+
+
+
+}
